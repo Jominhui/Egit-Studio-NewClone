dffe6f32fc63520bbe66c956660365adfc4bc2d4
Jominhui <rihito2021@naver.com>
2021-04-08 10:50:59 +0900

    UPDATE : Router

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,23 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import MainPage from "../pages/MainPage";
+import Header from "../components/common/Header";
+import ArtistPage from "../pages/ArtistPage";
 
 const App = () => {
   return (
-    <div className="App">
-      <Switch>
-        <Route exact path="/" component={MainPage} />
-      </Switch>
-    </div>
+    <>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route exact path="/" component={MainPage} />
+          <Route exact path="/game" component={MainPage} />
+          <Route exact path="/artist" component={ArtistPage} />
+          <Route exact path="/studio" component={MainPage} />
+          <Route exact path="/store" component={MainPage} />
+        </Switch>
+      </div>
+    </>
   );
 };
 
